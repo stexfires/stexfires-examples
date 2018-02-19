7c060d42310ef8b5eccf634e5d60e3b2b44c1475
Mathias Kalb <mkalb@mkalb.de>
2018-02-19 16:24:58 +0100

    Refactor and optimize ExamplesLogger

--- src/main/java/stexfires/examples/core/ExamplesLogger.java
+++ src/main/java/stexfires/examples/core/ExamplesLogger.java
@@ -2,9 +2,8 @@ package stexfires.examples.core;
 
 import stexfires.core.Record;
 import stexfires.core.RecordStreams;
-import stexfires.core.consumer.LoggerConsumer;
+import stexfires.core.consumer.NullConsumer;
 import stexfires.core.filter.ClassFilter;
-import stexfires.core.filter.RecordFilter;
 import stexfires.core.logger.AppendableLogger;
 import stexfires.core.logger.ConditionalLogger;
 import stexfires.core.logger.DispatcherLogger;
@@ -43,20 +42,24 @@ public final class ExamplesLogger {
         );
     }
 
+    private static void showLogger(String title, RecordLogger<Record> recordLogger) {
+        System.out.println("--" + title);
+        RecordStreams.log(generateStream(), recordLogger)
+                     .forEachOrdered(new NullConsumer<>().asConsumer());
+    }
+
     private static void showAppendableLogger() {
         System.out.println("-showAppendableLogger---");
 
-        System.out.println("constructor StringBuilder");
         StringBuilder builder = new StringBuilder(10);
-        RecordStreams.consume(generateStream(), new LoggerConsumer<>(
-                new AppendableLogger<>(builder, new SizeMessage<>())));
+        showLogger("constructor StringBuilder",
+                new AppendableLogger<>(builder, new SizeMessage<>()));
         System.out.println(builder.toString());
 
-        System.out.println("constructor StringWriter");
         try {
             try (StringWriter writer = new StringWriter(10)) {
-                RecordStreams.consume(generateStream(), new LoggerConsumer<>(
-                        new AppendableLogger<>(writer, new SizeMessage<>())));
+                showLogger("constructor StringWriter",
+                        new AppendableLogger<>(writer, new SizeMessage<>()));
                 System.out.println(writer.toString());
             }
         } catch (IOException e) {
@@ -67,94 +70,88 @@ public final class ExamplesLogger {
     private static void showConditionalLogger() {
         System.out.println("-showConditionalLogger---");
 
-        RecordStreams.consume(generateStream(), new LoggerConsumer<>(
-                new ConditionalLogger<>(ClassFilter.equalTo(StandardRecord.class),
-                        new SystemOutLogger<>(), new NullLogger<>())));
+        showLogger("constructor",
+                new ConditionalLogger<>(
+                        ClassFilter.equalTo(StandardRecord.class),
+                        new SystemOutLogger<>(),
+                        new NullLogger<>()));
     }
 
     private static void showDispatcherLogger() {
         System.out.println("-showDispatcherLogger---");
 
-        System.out.println("-bySize---");
-        List<RecordLogger<? super Record>> recordLoggersSize = new ArrayList<>();
+        List<SystemOutLogger<Record>> recordLoggersSize = new ArrayList<>();
         recordLoggersSize.add(new SystemOutLogger<>("Size 0: "));
         recordLoggersSize.add(new SystemOutLogger<>("Size 1: "));
         recordLoggersSize.add(new SystemOutLogger<>("Size 2: "));
-        RecordStreams.consume(generateStream(), new LoggerConsumer<>(
-                DispatcherLogger.bySize(recordLoggersSize)));
 
-        System.out.println("-byFilters---");
-        List<RecordFilter<? super Record>> recordFilters = new ArrayList<>();
+        showLogger("bySize",
+                DispatcherLogger.bySize(recordLoggersSize));
+
+        List<ClassFilter<Record>> recordFilters = new ArrayList<>();
         recordFilters.add(ClassFilter.equalTo(EmptyRecord.class));
         recordFilters.add(ClassFilter.equalTo(SingleRecord.class));
         recordFilters.add(ClassFilter.equalTo(PairRecord.class));
+
         List<RecordLogger<? super Record>> recordLoggersFilter = new ArrayList<>();
         recordLoggersFilter.add(new SystemOutLogger<>("Filter EmptyRecord: "));
         recordLoggersFilter.add(new SystemOutLogger<>("Filter SingleRecord: "));
         recordLoggersFilter.add(new SystemOutLogger<>("Filter PairRecord: "));
-        RecordStreams.consume(generateStream(), new LoggerConsumer<>(
-                DispatcherLogger.byFilters(recordFilters, recordLoggersFilter)));
+
+        showLogger("byFilters",
+                DispatcherLogger.byFilters(recordFilters, recordLoggersFilter));
     }
 
     private static void showLimitedLogger() {
         System.out.println("-showLimitedLogger---");
 
-        RecordStreams.consume(generateStream(), new LoggerConsumer<>(
-                new LimitedLogger<>(new SystemOutLogger<>(), 2)));
+        showLogger("constructor 0",
+                new LimitedLogger<>(new SystemOutLogger<>(), 0));
+        showLogger("constructor 2",
+                new LimitedLogger<>(new SystemOutLogger<>(), 2));
     }
 
     private static void showNullLogger() {
         System.out.println("-showNullLogger---");
 
-        RecordStreams.consume(generateStream(), new LoggerConsumer<>(
-                new NullLogger<>()));
+        showLogger("constructor",
+                new NullLogger<>());
     }
 
     private static void showPrintStreamLogger() {
         System.out.println("-showPrintStreamLogger---");
 
-        RecordStreams.consume(generateStream(), new LoggerConsumer<>(
-                new PrintStreamLogger<>(System.out, new SizeMessage<>())));
+        showLogger("constructor",
+                new PrintStreamLogger<>(System.out, new SizeMessage<>()));
     }
 
     private static void showRecordLogger() {
         System.out.println("-showRecordLogger---");
 
-        System.out.println("concat 2");
-        RecordStreams.consume(generateStream(), new LoggerConsumer<>(
-                RecordLogger.concat(new SystemOutLogger<>(), new NullLogger<>())));
-
-        System.out.println("concat 3");
-        RecordStreams.consume(generateStream(), new LoggerConsumer<>(
-                RecordLogger.concat(new SystemOutLogger<>(), new NullLogger<>(), new SystemOutLogger<>())));
-
-        System.out.println("andThen");
-        RecordStreams.consume(generateStream(), new LoggerConsumer<>(
-                new NullLogger<>().andThen(new SystemOutLogger<>())));
+        showLogger("concat 2\"",
+                RecordLogger.concat(new SystemOutLogger<>(), new NullLogger<>()));
+        showLogger("concat 3",
+                RecordLogger.concat(new SystemOutLogger<>(), new NullLogger<>(), new SystemOutLogger<>()));
+        showLogger("andThen",
+                new NullLogger<>().andThen(new SystemOutLogger<>()));
     }
 
     private static void showSystemOutLogger() {
         System.out.println("-showSystemOutLogger---");
 
-        System.out.println("constructor prefix");
-        RecordStreams.consume(generateStream(), new LoggerConsumer<>(
-                new SystemOutLogger<>("--")));
-
-        System.out.println("constructor SizeMessage");
-        RecordStreams.consume(generateStream(), new LoggerConsumer<>(
-                new SystemOutLogger<>(new SizeMessage<>())));
+        showLogger("constructor prefix",
+                new SystemOutLogger<>("--"));
+        showLogger("constructor SizeMessage",
+                new SystemOutLogger<>(new SizeMessage<>()));
     }
 
     private static void showSystemErrLogger() {
         System.out.println("-showSystemErrLogger---");
 
-        System.out.println("constructor prefix");
-        RecordStreams.consume(generateStream(), new LoggerConsumer<>(
-                new SystemErrLogger<>("--")));
-
-        System.out.println("constructor SizeMessage");
-        RecordStreams.consume(generateStream(), new LoggerConsumer<>(
-                new SystemErrLogger<>(new SizeMessage<>())));
+        showLogger("constructor prefix",
+                new SystemErrLogger<>("--"));
+        showLogger("constructor SizeMessage",
+                new SystemErrLogger<>(new SizeMessage<>()));
     }
 
     public static void main(String[] args) {
