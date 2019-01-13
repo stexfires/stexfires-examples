e30140b42778ae220bfff0d80ce359bc552529d8
Mathias Kalb <mkalb@mkalb.de>
2019-01-13 07:36:06 +0100

    Switch to Gradle 5.1.1 and migrate to Kotlin DSL

---
+++ build.gradle.kts
@@ -0,0 +1,40 @@
+group = "stexfires"
+version = "0.1.0"
+
+plugins {
+    `java`
+    `project-report`
+}
+
+dependencies {
+    implementation("stexfires:stexfires:0.1.0")
+}
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_1_8
+}
+
+val sharedManifest = the<JavaPluginConvention>().manifest {
+    attributes(
+            "Implementation-Title" to project.name,
+            "Implementation-Version" to project.version
+    )
+}
+
+tasks {
+    jar {
+        manifest = project.the<JavaPluginConvention>().manifest {
+            from(sharedManifest)
+        }
+    }
+    javadoc {
+        options.encoding = "UTF-8"
+    }
+    compileJava {
+        options.encoding = "UTF-8"
+    }
+    compileTestJava {
+        options.encoding = "UTF-8"
+    }
+}
+
