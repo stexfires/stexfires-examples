607865a4b92436d03824c7ed8ab91b6cab1b8cd0
Mathias Kalb <mkalb@mkalb.de>
2019-03-19 21:39:31 +0100

    Add ExamplesCommonCharsetNames

---
+++ src/main/java/stexfires/examples/util/ExamplesCommonCharsetNames.java
@@ -0,0 +1,24 @@
+package stexfires.examples.util;
+
+import stexfires.util.CommonCharsetNames;
+
+public final class ExamplesCommonCharsetNames {
+
+    private ExamplesCommonCharsetNames() {
+    }
+
+    private static void showCommonCharsetNames() {
+        System.out.println("-showCommonCharsetNames---");
+
+        for (CommonCharsetNames type : CommonCharsetNames.values()) {
+            System.out.println(type.name() + " (getCanonicalName()) ? " + type.getCanonicalName());
+            System.out.println(type.name() + " (charset()) ? " + type.charset());
+            System.out.println(type.name() + " (toString()) ? " + type.toString());
+        }
+    }
+
+    public static void main(String[] args) {
+        showCommonCharsetNames();
+    }
+
+}
