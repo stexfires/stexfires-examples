decbb8e266247ff6d2dbf813b4c6d7cda051b941
Mathias Kalb <mkalb@mkalb.de>
2020-06-02 20:57:04 +0200

    Optimize ExamplesRecord

--- src/main/java/stexfires/examples/core/ExamplesRecord.java
+++ src/main/java/stexfires/examples/core/ExamplesRecord.java
@@ -1,5 +1,6 @@
 package stexfires.examples.core;
 
+import stexfires.core.Fields;
 import stexfires.core.Record;
 import stexfires.core.record.EmptyRecord;
 import stexfires.core.record.KeyValueRecord;
@@ -8,48 +9,98 @@ import stexfires.core.record.SingleRecord;
 import stexfires.core.record.StandardRecord;
 import stexfires.util.Strings;
 
+import java.util.Arrays;
+
+@SuppressWarnings("UseOfSystemOutOrSystemErr")
 public final class ExamplesRecord {
 
     private ExamplesRecord() {
     }
 
     private static void showRecord(Record record) {
-        System.out.println("toString:    " + record.toString());
-        System.out.println("hashCode:    " + record.hashCode());
-        System.out.println("size:        " + record.size());
-        System.out.println("isEmpty:     " + record.isEmpty());
-        System.out.println("hasCategory: " + record.hasCategory());
-        System.out.println("getCategory: " + record.getCategory());
-        System.out.println("hasRecordId: " + record.hasRecordId());
-        System.out.println("getRecordId: " + record.getRecordId());
+        System.out.println("arrayOfFields:             " + Arrays.toString(record.arrayOfFields()));
+        System.out.println("listOfFields:              " + record.listOfFields());
+        System.out.println("streamOfFields:            " + Fields.collectFields(record.streamOfFields()));
+        System.out.println("getCategory:               " + record.getCategory());
+        System.out.println("hasCategory:               " + record.hasCategory());
+        System.out.println("getCategoryOrElse:         " + record.getCategoryOrElse("else"));
+        System.out.println("getCategoryAsOptional:     " + record.getCategoryAsOptional());
+        System.out.println("streamOfCategory:          " + record.streamOfCategory().findFirst());
+        System.out.println("getRecordId:               " + record.getRecordId());
+        System.out.println("hasRecordId:               " + record.hasRecordId());
+        System.out.println("getRecordIdAsOptionalLong: " + record.getRecordIdAsOptionalLong());
+        System.out.println("getRecordIdAsOptional:     " + record.getRecordIdAsOptional());
+        System.out.println("streamOfRecordId:          " + record.streamOfRecordId().findFirst());
+        System.out.println("size:                      " + record.size());
+        System.out.println("isEmpty:                   " + record.isEmpty());
+        System.out.println("isValidIndex:              " + record.isValidIndex(0));
+        System.out.println("getFieldAt:                " + record.getFieldAt(0));
+        System.out.println("getFirstField:             " + record.getFirstField());
+        System.out.println("getLastField:              " + record.getLastField());
+        System.out.println("getValueAt:                " + record.getValueAt(0));
+        System.out.println("getValueAtOrElse:          " + record.getValueAtOrElse(0, "else"));
+        System.out.println("getValueOfFirstField:      " + record.getValueOfFirstField());
+        System.out.println("getValueOfLastField:       " + record.getValueOfLastField());
+        System.out.println("hashCode:                  " + record.hashCode());
+        System.out.println("toString:                  " + record.toString());
         System.out.println();
     }
 
     private static void showEmptyRecord() {
         System.out.println("-showEmptyRecord---");
 
-        showRecord(new EmptyRecord());
+        EmptyRecord record = new EmptyRecord();
+        showRecord(record);
     }
 
     private static void showKeyValueRecord() {
         System.out.println("-showKeyValueRecord---");
 
-        showRecord(new KeyValueRecord("key", "value"));
-        showRecord(new KeyValueRecord("category", 0L, "key", "value"));
+        KeyValueRecord record = new KeyValueRecord("key", "value");
+        System.out.println("getKeyField:               " + record.getKeyField());
+        System.out.println("getValueOfKeyField:        " + record.getValueOfKeyField());
+        System.out.println("getValueField:             " + record.getValueField());
+        System.out.println("getValueOfValueField:      " + record.getValueOfValueField());
+        System.out.println("newValueRecord:            " + record.newValueRecord("new value"));
+        showRecord(record);
+
+        KeyValueRecord record1 = new KeyValueRecord("category", 0L, "key", "value");
+        System.out.println("getKeyField:               " + record1.getKeyField());
+        System.out.println("getValueOfKeyField:        " + record1.getValueOfKeyField());
+        System.out.println("getValueField:             " + record1.getValueField());
+        System.out.println("getValueOfValueField:      " + record1.getValueOfValueField());
+        System.out.println("newValueRecord:            " + record1.newValueRecord("new value"));
+        showRecord(record1);
     }
 
     private static void showPairRecord() {
         System.out.println("-showPairRecord---");
 
-        showRecord(new PairRecord("first", "second"));
-        showRecord(new PairRecord("category", 0L, "first", "second"));
+        PairRecord record = new PairRecord("first", "second");
+        System.out.println("newRecordSwapped:          " + record.newRecordSwapped());
+        System.out.println("getSecondField:            " + record.getSecondField());
+        System.out.println("getValueOfSecondField:     " + record.getValueOfSecondField());
+        showRecord(record);
+
+        PairRecord record1 = new PairRecord("category", 0L, "first", "second");
+        System.out.println("newRecordSwapped:          " + record1.newRecordSwapped());
+        System.out.println("getSecondField:            " + record1.getSecondField());
+        System.out.println("getValueOfSecondField:     " + record1.getValueOfSecondField());
+        showRecord(record1);
     }
 
     private static void showSingleRecord() {
         System.out.println("-showSingleRecord---");
 
-        showRecord(new SingleRecord("value"));
-        showRecord(new SingleRecord("category", 0L, "value"));
+        SingleRecord record = new SingleRecord("value");
+        System.out.println("getValueField:             " + record.getValueField());
+        System.out.println("getValueOfValueField:      " + record.getValueOfValueField());
+        showRecord(record);
+
+        SingleRecord record1 = new SingleRecord("category", 0L, "value");
+        System.out.println("getValueField:             " + record1.getValueField());
+        System.out.println("getValueOfValueField:      " + record1.getValueOfValueField());
+        showRecord(record1);
     }
 
     private static void showStandardRecord() {
@@ -68,7 +119,6 @@ public final class ExamplesRecord {
         showRecord(new StandardRecord("value1", "value2", "value3"));
         showRecord(new StandardRecord("category", 0L, "value1", "value2", "value3"));
     }
-
 
     public static void main(String[] args) {
         showEmptyRecord();
