225b02beaf867ddb223e714b1f4026914e241289
mkalb <mkalb@mkalb.de>
2022-10-15 14:47:19 +0200

    Optimize AutoCloseableTest

--- src/main/java/stexfires/examples/javatest/AutoCloseableTest.java
+++ src/main/java/stexfires/examples/javatest/AutoCloseableTest.java
@@ -3,91 +3,108 @@ package stexfires.examples.javatest;
 @SuppressWarnings("UseOfSystemOutOrSystemErr")
 public final class AutoCloseableTest {
 
+    private enum ThrowRuntimeExceptionType {
+        NONE, CONSTRUCTOR, METHOD, CLOSE, METHOD_AND_CLOSE
+    }
+
     private AutoCloseableTest() {
     }
 
-    private static void showTestCase1() {
-        System.out.println("-showTestCase1---");
+    private static void testTryWithResource(ThrowRuntimeExceptionType throwRuntimeExceptionType) {
+        System.out.println("-testTryWithResource---");
         try {
-            try (TestClose t = new TestClose(false)) {
-                t.test(false);
+            try (TestCloseable t = new TestCloseable(throwRuntimeExceptionType)) {
+                t.method();
+            } catch (RuntimeException e) {
+                System.out.println("inner catch: " + e.getMessage() + (e.getSuppressed().length >= 1 ? " AND " + e.getSuppressed()[0].getMessage() : ""));
+            } finally {
+                System.out.println("inner finally");
             }
         } catch (RuntimeException e) {
-            System.out.println("catch " + e);
+            System.out.println("outer catch: " + e.getMessage() + (e.getSuppressed().length >= 1 ? " AND " + e.getSuppressed()[0].getMessage() : ""));
         }
+        System.out.println();
     }
 
-    private static void showTestCase2() {
-        System.out.println("-showTestCase2---");
+    @SuppressWarnings("UnnecessaryLocalVariable")
+    private static void testTryWithResourceConstructorBefore(ThrowRuntimeExceptionType throwRuntimeExceptionType) {
+        System.out.println("-testTryWithResourceConstructorBefore---");
         try {
-            try (TestClose t = new TestClose(true)) {
-                t.test(false);
+            // constructor before try-with
+            TestCloseable testCloseable = new TestCloseable(throwRuntimeExceptionType);
+            try (TestCloseable t = testCloseable) {
+                t.method();
+            } catch (RuntimeException e) {
+                System.out.println("inner catch: " + e.getMessage() + (e.getSuppressed().length >= 1 ? " AND " + e.getSuppressed()[0].getMessage() : ""));
+            } finally {
+                System.out.println("inner finally");
             }
         } catch (RuntimeException e) {
-            System.out.println("catch " + e);
+            System.out.println("outer catch: " + e.getMessage() + (e.getSuppressed().length >= 1 ? " AND " + e.getSuppressed()[0].getMessage() : ""));
         }
+        System.out.println();
     }
 
-    private static void showTestCase3() {
-        System.out.println("-showTestCase3---");
+    private static void testWithoutTryWithResource(ThrowRuntimeExceptionType throwRuntimeExceptionType) {
+        System.out.println("-testWithoutTryWithResource---");
         try {
-            try (TestClose t = new TestClose(false)) {
-                t.test(true);
+            TestCloseable t = new TestCloseable(throwRuntimeExceptionType);
+            try {
+                t.method();
+            } catch (RuntimeException e) {
+                System.out.println("inner catch: " + e.getMessage() + (e.getSuppressed().length >= 1 ? " AND " + e.getSuppressed()[0].getMessage() : ""));
+            } finally {
+                System.out.println("inner finally");
             }
+            t.close();
         } catch (RuntimeException e) {
-            System.out.println("catch " + e);
+            System.out.println("outer catch: " + e.getMessage() + (e.getSuppressed().length >= 1 ? " AND " + e.getSuppressed()[0].getMessage() : ""));
         }
-    }
-
-    @SuppressWarnings({"UnnecessaryLocalVariable", "resource"})
-    private static void showTestCase4() {
-        System.out.println("-showTestCase4---");
-        TestClose tBefore = new TestClose(false);
-        try {
-            TestClose t = tBefore;
-            t.test(true);
-        } catch (RuntimeException e) {
-            System.out.println("catch " + e);
-        }
-    }
-
-    private static void showTestCase5() {
-        System.out.println("-showTestCase5---");
-        TestClose tBefore = new TestClose(false);
-        try {
-            try (TestClose t = tBefore) {
-                t.test(true);
-            }
-        } catch (RuntimeException e) {
-            System.out.println("catch " + e);
-        }
+        System.out.println();
     }
 
     public static void main(String... args) {
-        showTestCase1();
-        showTestCase2();
-        showTestCase3();
-        showTestCase4();
-        showTestCase5();
+        testTryWithResource(ThrowRuntimeExceptionType.NONE);
+        testTryWithResource(ThrowRuntimeExceptionType.CONSTRUCTOR);
+        testTryWithResource(ThrowRuntimeExceptionType.METHOD);
+        testTryWithResource(ThrowRuntimeExceptionType.CLOSE);
+        testTryWithResource(ThrowRuntimeExceptionType.METHOD_AND_CLOSE);
+
+        testTryWithResourceConstructorBefore(ThrowRuntimeExceptionType.CONSTRUCTOR);
+        testTryWithResourceConstructorBefore(ThrowRuntimeExceptionType.METHOD);
+
+        testWithoutTryWithResource(ThrowRuntimeExceptionType.CONSTRUCTOR);
+        testWithoutTryWithResource(ThrowRuntimeExceptionType.METHOD);
+        testWithoutTryWithResource(ThrowRuntimeExceptionType.CLOSE);
+        testWithoutTryWithResource(ThrowRuntimeExceptionType.METHOD_AND_CLOSE);
     }
 
-    private static class TestClose implements AutoCloseable {
+    @SuppressWarnings({"ClassCanBeRecord", "UseOfSystemOutOrSystemErr"})
+    private static final class TestCloseable implements AutoCloseable {
 
-        TestClose(boolean ex) {
-            System.out.println("constructor " + ex);
-            if (ex) throw new RuntimeException("constructor");
+        private final ThrowRuntimeExceptionType throwRuntimeExceptionType;
+
+        private TestCloseable(ThrowRuntimeExceptionType throwRuntimeExceptionType) {
+            this.throwRuntimeExceptionType = throwRuntimeExceptionType;
+
+            System.out.println("constructor " + throwRuntimeExceptionType);
+            if (throwRuntimeExceptionType == ThrowRuntimeExceptionType.CONSTRUCTOR)
+                throw new RuntimeException("Ex: 'constructor'");
         }
 
-        @SuppressWarnings("MethodMayBeStatic")
-        void test(boolean ex) {
-            System.out.println("test " + ex);
-            if (ex) throw new RuntimeException("test");
+        private void method() {
+            System.out.println("method " + throwRuntimeExceptionType);
+            if (throwRuntimeExceptionType == ThrowRuntimeExceptionType.METHOD || throwRuntimeExceptionType == ThrowRuntimeExceptionType.METHOD_AND_CLOSE)
+                throw new RuntimeException("Ex: 'method'");
         }
 
         @Override
         public void close() {
-            System.out.println("close");
+            System.out.println("close " + throwRuntimeExceptionType);
+            if (throwRuntimeExceptionType == ThrowRuntimeExceptionType.CLOSE || throwRuntimeExceptionType == ThrowRuntimeExceptionType.METHOD_AND_CLOSE)
+                throw new RuntimeException("Ex: 'close'");
         }
+
     }
 
 }
