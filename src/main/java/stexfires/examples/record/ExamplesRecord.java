7f1625c996e47bec0f5a3e804f3c49f7e6743ecb
mkalb <mkalb@mkalb.de>
2022-11-26 12:27:24 +0100

    Optimize ExamplesRecord

--- src/main/java/stexfires/examples/record/ExamplesRecord.java
+++ src/main/java/stexfires/examples/record/ExamplesRecord.java
@@ -1,17 +1,14 @@
 package stexfires.examples.record;
 
 import stexfires.record.KeyValueRecord;
-import stexfires.record.TextFields;
-import stexfires.record.TextRecord;
 import stexfires.record.TextRecords;
 import stexfires.record.ValueRecord;
+import stexfires.record.impl.KeyValueCommentFieldsRecord;
 import stexfires.record.impl.KeyValueFieldsRecord;
 import stexfires.record.impl.ManyFieldsRecord;
 import stexfires.record.impl.TwoFieldsRecord;
 import stexfires.record.impl.ValueFieldRecord;
 import stexfires.util.Strings;
-
-import java.util.Arrays;
 
 @SuppressWarnings("UseOfSystemOutOrSystemErr")
 public final class ExamplesRecord {
@@ -19,114 +16,209 @@ public final class ExamplesRecord {
     private ExamplesRecord() {
     }
 
-    private static void printRecord(TextRecord record) {
-        System.out.println("arrayOfFields:             " + Arrays.toString(record.arrayOfFields()));
-        System.out.println("listOfFields:              " + record.listOfFields());
-        System.out.println("listOfFieldsReversed:      " + record.listOfFieldsReversed());
-        System.out.println("streamOfFields:            " + TextFields.collectFields(record.streamOfFields()));
-        System.out.println("category:                  " + record.category());
-        System.out.println("hasCategory:               " + record.hasCategory());
-        System.out.println("categoryAsOptional:        " + record.categoryAsOptional());
-        System.out.println("categoryAsStream:          " + record.categoryAsStream().findFirst());
-        System.out.println("recordId:                  " + record.recordId());
-        System.out.println("hasRecordId:               " + record.hasRecordId());
-        System.out.println("recordIdAsOptionalLong:    " + record.recordIdAsOptionalLong());
-        System.out.println("recordIdAsOptional:        " + record.recordIdAsOptional());
-        System.out.println("recordIdAsLongStream:          " + record.recordIdAsLongStream().findFirst());
-        System.out.println("size:                      " + record.size());
-        System.out.println("isEmpty:                   " + record.isEmpty());
-        System.out.println("isValidIndex:              " + record.isValidIndex(0));
-        System.out.println("fieldAt:                   " + record.fieldAt(0));
-        System.out.println("firstField:                " + record.firstField());
-        System.out.println("lastField:                 " + record.lastField());
-        System.out.println("textAt:                   " + record.textAt(0));
-        System.out.println("textAtOrElse:             " + record.textAtOrElse(0, "else"));
-        System.out.println("valueOfFirstField:         " + record.firstText());
-        System.out.println("valueOfLastField:          " + record.lastText());
-        System.out.println("hashCode:                  " + record.hashCode());
-        System.out.println("toString:                  " + record);
-        System.out.println();
-    }
-
     private static void showEmptyRecord() {
         System.out.println("-showEmptyRecord---");
 
-        printRecord(TextRecords.empty());
+        RecordSystemOutUtil.printlnRecordExtended(TextRecords.empty());
+        System.out.println();
     }
 
-    private static void showKeyValueRecord() {
-        System.out.println("-showKeyValueRecord---");
+    private static void showKeyValueCommentFieldsRecord() {
+        System.out.println("-showKeyValueCommentFieldsRecord---");
 
-        KeyValueRecord record = new KeyValueFieldsRecord("key", "value");
-        System.out.println("keyField:                  " + record.keyField());
-        System.out.println("key:                       " + record.key());
-        System.out.println("valueField:                " + record.valueField());
-        System.out.println("value:                     " + record.value());
-        System.out.println("withKey:                   " + record.withKey("newKey"));
-        System.out.println("withValue:                 " + record.withValue("new value"));
-        printRecord(record);
-
-        KeyValueRecord record1 = new KeyValueFieldsRecord("category", 0L, "key", "value");
-        System.out.println("keyField:                  " + record1.keyField());
-        System.out.println("key:                       " + record1.key());
-        System.out.println("valueField:                " + record1.valueField());
-        System.out.println("value:                     " + record1.value());
-        System.out.println("withKey:                   " + record1.withKey("newKey"));
-        System.out.println("withValue:                 " + record1.withValue("new value"));
-        printRecord(record1);
+        {
+            KeyValueCommentFieldsRecord record = new KeyValueCommentFieldsRecord("key", "value", "comment");
+            RecordSystemOutUtil.printlnRecordExtended(record);
+            System.out.println("keyField:                  " + record.keyField());
+            System.out.println("key:                       " + record.key());
+            System.out.println("keyIndex:                  " + record.keyIndex());
+            System.out.println("withKey:                   " + record.withKey("newKey"));
+            System.out.println("valueField:                " + record.valueField());
+            System.out.println("value:                     " + record.value());
+            System.out.println("valueIndex:                " + record.valueIndex());
+            System.out.println("valueAsOptional:           " + record.valueAsOptional());
+            System.out.println("withValue:                 " + record.withValue("new value"));
+            System.out.println("commentField:              " + record.commentField());
+            System.out.println("comment:                   " + record.comment());
+            System.out.println("commentIndex:              " + record.commentIndex());
+            System.out.println("commentAsOptional:         " + record.commentAsOptional());
+            System.out.println("withComment:               " + record.withComment("new comment"));
+            System.out.println();
+        }
+        {
+            KeyValueCommentFieldsRecord record = new KeyValueCommentFieldsRecord("category", 0L, "key", "value", "comment");
+            RecordSystemOutUtil.printlnRecordExtended(record);
+            System.out.println("keyField:                  " + record.keyField());
+            System.out.println("key:                       " + record.key());
+            System.out.println("keyIndex:                  " + record.keyIndex());
+            System.out.println("withKey:                   " + record.withKey("newKey"));
+            System.out.println("valueField:                " + record.valueField());
+            System.out.println("value:                     " + record.value());
+            System.out.println("valueIndex:                " + record.valueIndex());
+            System.out.println("valueAsOptional:           " + record.valueAsOptional());
+            System.out.println("withValue:                 " + record.withValue("new value"));
+            System.out.println("commentField:              " + record.commentField());
+            System.out.println("comment:                   " + record.comment());
+            System.out.println("commentIndex:              " + record.commentIndex());
+            System.out.println("commentAsOptional:         " + record.commentAsOptional());
+            System.out.println("withComment:               " + record.withComment("new comment"));
+            System.out.println();
+        }
+        {
+            KeyValueCommentFieldsRecord record = new KeyValueCommentFieldsRecord("key", null, null);
+            RecordSystemOutUtil.printlnRecordExtended(record);
+            System.out.println("keyField:                  " + record.keyField());
+            System.out.println("key:                       " + record.key());
+            System.out.println("keyIndex:                  " + record.keyIndex());
+            System.out.println("withKey:                   " + record.withKey("newKey"));
+            System.out.println("valueField:                " + record.valueField());
+            System.out.println("value:                     " + record.value());
+            System.out.println("valueIndex:                " + record.valueIndex());
+            System.out.println("valueAsOptional:           " + record.valueAsOptional());
+            System.out.println("withValue:                 " + record.withValue("new value"));
+            System.out.println("commentField:              " + record.commentField());
+            System.out.println("comment:                   " + record.comment());
+            System.out.println("commentIndex:              " + record.commentIndex());
+            System.out.println("commentAsOptional:         " + record.commentAsOptional());
+            System.out.println("withComment:               " + record.withComment("new comment"));
+            System.out.println();
+        }
     }
 
-    private static void showTwoValuesRecord() {
-        System.out.println("-showTwoValuesRecord---");
+    private static void showKeyValueFieldsRecord() {
+        System.out.println("-showKeyValueFieldsRecord---");
 
-        TwoFieldsRecord record = new TwoFieldsRecord("first", "second");
-        System.out.println("withSwappedTexts:          " + record.withSwappedTexts());
-        System.out.println("secondField:               " + record.secondField());
-        System.out.println("valueOfSecondField:        " + record.secondText());
-        printRecord(record);
-
-        TwoFieldsRecord record1 = new TwoFieldsRecord("category", 0L, "first", "second");
-        System.out.println("withSwappedTexts:          " + record1.withSwappedTexts());
-        System.out.println("secondField:               " + record1.secondField());
-        System.out.println("valueOfSecondField:        " + record1.secondText());
-        printRecord(record1);
+        {
+            KeyValueRecord record = new KeyValueFieldsRecord("key", "value");
+            RecordSystemOutUtil.printlnRecordExtended(record);
+            System.out.println("keyField:                  " + record.keyField());
+            System.out.println("key:                       " + record.key());
+            System.out.println("keyIndex:                  " + record.keyIndex());
+            System.out.println("withKey:                   " + record.withKey("newKey"));
+            System.out.println("valueField:                " + record.valueField());
+            System.out.println("value:                     " + record.value());
+            System.out.println("valueIndex:                " + record.valueIndex());
+            System.out.println("valueAsOptional:           " + record.valueAsOptional());
+            System.out.println("withValue:                 " + record.withValue("new value"));
+            System.out.println();
+        }
+        {
+            KeyValueRecord record = new KeyValueFieldsRecord("category", 0L, "key", "value");
+            RecordSystemOutUtil.printlnRecordExtended(record);
+            System.out.println("keyField:                  " + record.keyField());
+            System.out.println("key:                       " + record.key());
+            System.out.println("keyIndex:                  " + record.keyIndex());
+            System.out.println("withKey:                   " + record.withKey("newKey"));
+            System.out.println("valueField:                " + record.valueField());
+            System.out.println("value:                     " + record.value());
+            System.out.println("valueIndex:                " + record.valueIndex());
+            System.out.println("valueAsOptional:           " + record.valueAsOptional());
+            System.out.println("withValue:                 " + record.withValue("new value"));
+            System.out.println();
+        }
+        {
+            KeyValueRecord record = new KeyValueFieldsRecord("key", null);
+            RecordSystemOutUtil.printlnRecordExtended(record);
+            System.out.println("keyField:                  " + record.keyField());
+            System.out.println("key:                       " + record.key());
+            System.out.println("keyIndex:                  " + record.keyIndex());
+            System.out.println("withKey:                   " + record.withKey("newKey"));
+            System.out.println("valueField:                " + record.valueField());
+            System.out.println("value:                     " + record.value());
+            System.out.println("valueIndex:                " + record.valueIndex());
+            System.out.println("valueAsOptional:           " + record.valueAsOptional());
+            System.out.println("withValue:                 " + record.withValue("new value"));
+            System.out.println();
+        }
     }
 
-    private static void showOneValueRecord() {
-        System.out.println("-showOneValueRecord---");
+    private static void showManyFieldsRecord() {
+        System.out.println("-showManyFieldsRecord---");
 
-        ValueRecord record = new ValueFieldRecord("value");
-        System.out.println("valueField:                " + record.valueField());
-        System.out.println("value:                     " + record.value());
-        printRecord(record);
+        RecordSystemOutUtil.printlnRecordExtended(new ManyFieldsRecord());
 
-        ValueRecord record1 = new ValueFieldRecord("category", 0L, "value");
-        System.out.println("valueField:                " + record1.valueField());
-        System.out.println("value:                     " + record1.value());
-        printRecord(record1);
+        RecordSystemOutUtil.printlnRecordExtended(new ManyFieldsRecord(Strings.list("value1", "value2", "value3")));
+        RecordSystemOutUtil.printlnRecordExtended(new ManyFieldsRecord("category", 0L, Strings.list("value1", "value2", "value3")));
+
+        RecordSystemOutUtil.printlnRecordExtended(new ManyFieldsRecord(Strings.stream("value1", "value2", "value3")));
+        RecordSystemOutUtil.printlnRecordExtended(new ManyFieldsRecord("category", 0L, Strings.stream("value1", "value2", "value3")));
+
+        RecordSystemOutUtil.printlnRecordExtended(new ManyFieldsRecord("value1", "value2", "value3"));
+        RecordSystemOutUtil.printlnRecordExtended(new ManyFieldsRecord("category", 0L, "value1", "value2", "value3"));
+        System.out.println();
     }
 
-    private static void showManyValuesRecord() {
-        System.out.println("-showManyValuesRecord---");
+    private static void showTwoFieldsRecord() {
+        System.out.println("-showTwoFieldsRecord---");
 
-        printRecord(new ManyFieldsRecord());
+        {
+            TwoFieldsRecord record = new TwoFieldsRecord("first", "second");
+            RecordSystemOutUtil.printlnRecordExtended(record);
+            System.out.println("secondField:               " + record.secondField());
+            System.out.println("secondText:                " + record.secondText());
+            System.out.println("withSwappedTexts:          " + record.withSwappedTexts());
+            System.out.println();
+        }
+        {
+            TwoFieldsRecord record = new TwoFieldsRecord("category", 0L, "first", "second");
+            RecordSystemOutUtil.printlnRecordExtended(record);
+            System.out.println("secondField:               " + record.secondField());
+            System.out.println("secondText:                " + record.secondText());
+            System.out.println("withSwappedTexts:          " + record.withSwappedTexts());
+            System.out.println();
+        }
+        {
+            TwoFieldsRecord record = new TwoFieldsRecord(null, null);
+            RecordSystemOutUtil.printlnRecordExtended(record);
+            System.out.println("secondField:               " + record.secondField());
+            System.out.println("secondText:                " + record.secondText());
+            System.out.println("withSwappedTexts:          " + record.withSwappedTexts());
+            System.out.println();
+        }
+    }
 
-        printRecord(new ManyFieldsRecord(Strings.list("value1", "value2", "value3")));
-        printRecord(new ManyFieldsRecord("category", 0L, Strings.list("value1", "value2", "value3")));
+    private static void showValueFieldRecord() {
+        System.out.println("-showValueFieldRecord---");
 
-        printRecord(new ManyFieldsRecord(Strings.stream("value1", "value2", "value3")));
-        printRecord(new ManyFieldsRecord("category", 0L, Strings.stream("value1", "value2", "value3")));
-
-        printRecord(new ManyFieldsRecord("value1", "value2", "value3"));
-        printRecord(new ManyFieldsRecord("category", 0L, "value1", "value2", "value3"));
+        {
+            ValueRecord record = new ValueFieldRecord("value");
+            RecordSystemOutUtil.printlnRecordExtended(record);
+            System.out.println("valueField:                " + record.valueField());
+            System.out.println("value:                     " + record.value());
+            System.out.println("valueIndex:                " + record.valueIndex());
+            System.out.println("valueAsOptional:           " + record.valueAsOptional());
+            System.out.println("withValue:                 " + record.withValue("new value"));
+            System.out.println();
+        }
+        {
+            ValueRecord record = new ValueFieldRecord("category", 0L, "value");
+            RecordSystemOutUtil.printlnRecordExtended(record);
+            System.out.println("valueField:                " + record.valueField());
+            System.out.println("value:                     " + record.value());
+            System.out.println("valueIndex:                " + record.valueIndex());
+            System.out.println("valueAsOptional:           " + record.valueAsOptional());
+            System.out.println("withValue:                 " + record.withValue("new value"));
+            System.out.println();
+        }
+        {
+            ValueRecord record = new ValueFieldRecord(null);
+            RecordSystemOutUtil.printlnRecordExtended(record);
+            System.out.println("valueField:                " + record.valueField());
+            System.out.println("value:                     " + record.value());
+            System.out.println("valueIndex:                " + record.valueIndex());
+            System.out.println("valueAsOptional:           " + record.valueAsOptional());
+            System.out.println("withValue:                 " + record.withValue("new value"));
+            System.out.println();
+        }
     }
 
     public static void main(String... args) {
         showEmptyRecord();
-        showKeyValueRecord();
-        showTwoValuesRecord();
-        showOneValueRecord();
-        showManyValuesRecord();
+        showKeyValueCommentFieldsRecord();
+        showKeyValueFieldsRecord();
+        showManyFieldsRecord();
+        showTwoFieldsRecord();
+        showValueFieldRecord();
     }
 
 }
