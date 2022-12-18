bab3b7f8a6ff2ecd95cd06cc3e0abdb2f31a1b76
mkalb <mkalb@mkalb.de>
2022-12-18 15:40:42 +0100

    Add 'ofStandardCharset'

--- src/main/java/stexfires/examples/util/ExamplesCommonCharsetNames.java
+++ src/main/java/stexfires/examples/util/ExamplesCommonCharsetNames.java
@@ -2,6 +2,8 @@ package stexfires.examples.util;
 
 import stexfires.util.CommonCharsetNames;
 
+import java.nio.charset.StandardCharsets;
+
 @SuppressWarnings("UseOfSystemOutOrSystemErr")
 public final class ExamplesCommonCharsetNames {
 
@@ -15,6 +17,10 @@ public final class ExamplesCommonCharsetNames {
             System.out.println(charset.name() + " (canonicalName())    ? " + charset.canonicalName());
             System.out.println(charset.name() + " (charset())          ? " + charset.charset());
         }
+
+        System.out.println();
+        System.out.println("US_ASCII : " + CommonCharsetNames.ofStandardCharset(StandardCharsets.US_ASCII));
+        System.out.println("UTF_8    : " + CommonCharsetNames.ofStandardCharset(StandardCharsets.UTF_8));
     }
 
     public static void main(String... args) {
