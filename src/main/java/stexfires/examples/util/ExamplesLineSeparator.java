8a5992b5034945cb4df54aa84c0b6bcc59338521
mkalb <mkalb@mkalb.de>
2022-12-19 08:23:29 +0100

    Add 'length' and 'codePoints' methods

--- src/main/java/stexfires/examples/util/ExamplesLineSeparator.java
+++ src/main/java/stexfires/examples/util/ExamplesLineSeparator.java
@@ -15,12 +15,14 @@ public final class ExamplesLineSeparator {
         System.out.println("string count 3: " + LineSeparator.systemLineSeparator().string(3));
 
         for (var separator : LineSeparator.values()) {
-            System.out.println(separator.name() + " (string())        ? " + separator.string());
-            System.out.println(separator.name() + " (supplier() get)  ? " + separator.supplier().get());
-            System.out.println(separator.name() + " (stream() toList) ? " + separator.stream().toList());
-            System.out.println(separator.name() + " (chars() toList)  ? " + separator.chars().boxed().toList());
-            System.out.println(separator.name() + " (name())          ? " + separator.name());
-            System.out.println(separator.name() + " (toString())      ? " + separator);
+            System.out.println(separator.name() + " (string())             ? " + separator.string());
+            System.out.println(separator.name() + " (length())             ? " + separator.length());
+            System.out.println(separator.name() + " (supplier() get)       ? " + separator.supplier().get());
+            System.out.println(separator.name() + " (stream() toList)      ? " + separator.stream().toList());
+            System.out.println(separator.name() + " (chars() toList)       ? " + separator.chars().boxed().toList());
+            System.out.println(separator.name() + " (codePoints() toList)  ? " + separator.codePoints().boxed().toList());
+            System.out.println(separator.name() + " (name())               ? " + separator.name());
+            System.out.println(separator.name() + " (toString())           ? " + separator);
         }
     }
 
