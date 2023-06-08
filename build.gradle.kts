ea9b829303351aebd7723fc4b7fec2b38879550d
mkalb <mkalb@mkalb.de>
2023-06-08 14:26:17 +0200

    Update to gradle-8.1.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,40 +9,48 @@ repositories {
 }
 
 dependencies {
-    implementation("org.jetbrains:annotations:23.0.0")
+    implementation("org.jetbrains:annotations:24.0.1")
     implementation("stexfires:stexfires:0.1.0")
 }
 
+testing {
+    suites {
+        // Configure the built-in test suite
+        val test by getting(JvmTestSuite::class) {
+            // Use JUnit Jupiter test framework
+            useJUnitJupiter("5.9.3")
+        }
+    }
+}
+
+// Apply a specific Java toolchain to ease working on different environments.
 java {
     toolchain {
         languageVersion.set(JavaLanguageVersion.of(19))
     }
 }
 
+tasks.withType<JavaCompile>().configureEach {
+    options.compilerArgs.add("--enable-preview")
+    // options.compilerArgs.add("-Xlint:preview")
+    //options.compilerArgs.add("-Xlint:unchecked")
+    //options.compilerArgs.add("-Xlint:deprecation")
+}
+
+tasks.withType<Test>().configureEach {
+    jvmArgs("--enable-preview")
+}
+
+tasks.withType<JavaExec>().configureEach {
+    jvmArgs("--enable-preview")
+}
+
+tasks.withType<Javadoc> {
+    val javadocOptions = options as CoreJavadocOptions
+
+    javadocOptions.addStringOption("source", "19")
+    javadocOptions.addBooleanOption("-enable-preview", true)
+}
+
 group = "stexfires"
 version = "0.1.0"
-
-val sharedManifest = the<JavaPluginConvention>().manifest {
-    attributes(
-            "Implementation-Title" to project.name,
-            "Implementation-Version" to project.version
-    )
-}
-
-tasks {
-    jar {
-        manifest = project.the<JavaPluginConvention>().manifest {
-            from(sharedManifest)
-        }
-    }
-    javadoc {
-        options.encoding = "UTF-8"
-    }
-    compileJava {
-        options.encoding = "UTF-8"
-    }
-    compileTestJava {
-        options.encoding = "UTF-8"
-    }
-}
-
